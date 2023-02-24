c54bde1754c96cc48a3884e16ab86cf0246b320f
nazarchykur <nazarchykur@gmail.com>
2023-02-24 17:44:18 +0200

    init

---
+++ src/test/java/com/example/gittutorial/GitTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
