a62d739929d7be5b02a6f02fc2c150799caf8bde
nazarchykur <nazarchykur@gmail.com>
2023-02-27 15:46:34 +0200

    studying add / commit, commit --amend (with/without rewriting msg) / status

---
+++ src/main/java/com/example/gittutorial/lesson2/Lesson2.java
@@ -0,0 +1,23 @@
+package com.example.gittutorial.lesson2;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Lesson2 {
+    private String field1;
+    
+    private String field2;
+    
+    /*
+        studying add / commit, commit --amend (with/without rewriting msg) / status
+        
+        add some changes then add it to staging area
+        remove some changes and then add it to staging area
+        rewrite msg in commit message section using Intellij / console
+        
+     */
+}
