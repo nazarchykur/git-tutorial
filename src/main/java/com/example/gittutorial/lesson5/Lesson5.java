064aa3c0bdce54651405608ce8ebfbda57e4598c
nazarchykur <nazarchykur@gmail.com>
2023-02-28 15:25:30 +0200

    added commands git branch

--- src/main/java/com/example/gittutorial/lesson5/Lesson5.java
+++ src/main/java/com/example/gittutorial/lesson5/Lesson5.java
@@ -1,5 +1,33 @@
 package com.example.gittutorial.lesson5;
 
 public class Lesson5 {
+    /*
     
+    git branch
+        
+        git branch (-m | -M) [<oldbranch>] <newbranch>  : 
+                -m
+                --move
+                Move/rename a branch, together with its config and reflog.
+                
+                -M
+                Shortcut for --move --force.
+        
+        
+        git branch (-c | -C) [<oldbranch>] <newbranch> : 
+                -c
+                --copy
+                Copy a branch, together with its config and reflog.
+        
+        
+        git branch (-d | -D) [-r] <branchname> : 
+                -d
+                --delete
+                Delete a branch. The branch must be fully merged in its upstream branch, or in HEAD if no upstream was 
+                set with --track or --set-upstream-to.
+                
+                -D
+                Shortcut for --delete --force.
+
+     */
 }
