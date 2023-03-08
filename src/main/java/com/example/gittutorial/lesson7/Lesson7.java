fb77ee69388b3a17ca5ca8a3a65007d7196b1381
nazarchykur <nazarchykur@gmail.com>
2023-03-08 12:02:33 +0200

    added examples of git pull vs git pull --rebase on branch

---
+++ src/main/java/com/example/gittutorial/lesson7/Lesson7.java
@@ -0,0 +1,10 @@
+package com.example.gittutorial.lesson7;
+
+public class Lesson7 {
+    
+    /*
+        git pull          = git fetch + git merge against tracking upstream branch
+    
+        git pull --rebase = git fetch + git rebase against tracking upstream branch
+     */
+}
